52542957e64f84848d340560428a04eb0f4f2cd7
Saurelien <aurelienschmitt.info@gmail.com>
2022-09-24 12:21:35 +0200

    fixing flake8 version

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ from tournoi.controlers import HomeController
 from tournoi.models import Player, Tournament, tournament_list, players_list
 from tinydb import TinyDB
 
+"""créer une méthode pour load les joueurs et aussi pour les tournois load tournament"""
 
 players_list.append(Player.player_finalized(Player))
 tournament_list.append(Tournament.tournament_finalized(Tournament))
+print(players_list.append(Player.player_finalized(Player)))
 
 """tour = list()
 player_in_list = list()"""
